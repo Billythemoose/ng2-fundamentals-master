e043a07f82e61dc595ab0f57154f2af74d1a8597
v-mith <v-mith@microsoft.com>
2018-02-06 11:18:00 -0800

    stuff

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { AuthService } from './auth.service'
 import { Router } from '@angular/router'
 
@@ -16,8 +16,8 @@ export class ProfileComponent implements OnInit {
 
   // current error, navigating to profile page with no one logged in 
   ngOnInit(){
-    let firstName = new FormControl(this.authService.currentUser.firstName)
-    let lastName = new FormControl(this.authService.currentUser.lastName)
+    let firstName = new FormControl(this.authService.currentUser.firstName, Validators.required)
+    let lastName = new FormControl(this.authService.currentUser.lastName, Validators.required)
     this.profileForm = new FormGroup({
       firstName: firstName,
       lastName: lastName
@@ -25,8 +25,10 @@ export class ProfileComponent implements OnInit {
   }
 
   saveProfile(formValues){
-    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
-    this.router.navigate(['events'])
+    if (this.profileForm.valid){
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+      this.router.navigate(['events'])
+    }
   }
 
   cancel(){
